2dc1e6f3ec97cd55948adbbb0737d72d0168b06d
Jan Austerlitz <jaauster@microsoft.com>
2018-04-05 22:45:20 +0200

    winners

--- run_q_max.py
+++ run_q_max.py
@@ -10,7 +10,7 @@ from goblins import game
 
 # https://github.com/DanielSlater/AlphaToe
 def run():
-    player_1_wins = 0
+    winner = [0, 0, 0]
     for episode in range(1, 1000001):
         done = False
         player_1 = 1
@@ -25,8 +25,7 @@ def run():
                 moved, board, done, winner, info = game.make_move(
                     action, cur_player, board)  # 2
                 reward = _get_reward(moved, done, winner == cur_player)
-                player_1_wins += 1 if done and winner == cur_player else 0
-                a2.learn(old_board, board, action, reward)
+                a1.learn(old_board, board, action, reward)
             else:
                 # moved, board, done, winner, info = game.make_move(
                 #     randint(1, 9), player_2, board)
@@ -38,11 +37,14 @@ def run():
                 a2.learn(old_board, board, action, reward)
             #game.render(board)
             #time.sleep(0.1)
+            if done and cur_player:
+                winner[cur_player] = winner[cur_player] + 1
             if moved:
                 cur_player = player_1 if cur_player == player_2 else player_2
 
-        if episode % 100 == 0:
-            print('Episode {} player1 wins:{}'.format(episode, player_1_wins))
+
+        if episode % 1000 == 0:
+            print('Episode {} player1 wins:{}'.format(episode, winner[0]))
 
 
 def _get_reward(moved, done, winner):
